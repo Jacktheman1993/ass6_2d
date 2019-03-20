1ef9dd4af03bd19a916c471d701c61d5d70cf5de
Jacktheman1993 <32671071+Jacktheman1993@users.noreply.github.com>
2019-03-20 12:29:56 +0100

    first commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Enemy : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene("2D Platform game");
+            Debug.Log("DEAD");
+        }
+    }
+}
---
+++ Assets/Scripts/PowerUp.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Power up.... To defiet the enemy, jump over.......");
+            GetComponent<AudioSource>().Play();
+        }
+    }
+}
